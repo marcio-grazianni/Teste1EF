b89753894b9627a4cac8f1f6bc109d5ef90f1bf7
marcio <marcio.grazianni@gmail.com>
2024-09-11 18:44:10 -0300

    Indentação correta.

--- Data/Mappings/EstadoMap.cs
+++ Data/Mappings/EstadoMap.cs
@@ -4,60 +4,60 @@ using pratico.Models;
 
 namespace pratico.Data.Mappings
 {
-       public class EstadoMap : IEntityTypeConfiguration<Estado>
-       {
-              public void Configure(EntityTypeBuilder<Estado> builder)
-              {
-                     builder.ToTable("estado");
+    public class EstadoMap : IEntityTypeConfiguration<Estado>
+    {
+        public void Configure(EntityTypeBuilder<Estado> builder)
+        {
+            builder.ToTable("estado");
 
-                     builder.HasKey(e => e.Id)
-                            .HasName("pk_estado_id");
+            builder.HasKey(e => e.Id)
+                   .HasName("pk_estado_id");
 
-                     builder.Property(e => e.Id)
-                            .IsRequired()
-                            .HasColumnName("id")
-                            .HasColumnType("BIGINT")
-                            .ValueGeneratedOnAdd();
+            builder.Property(e => e.Id)
+                   .IsRequired()
+                   .HasColumnName("id")
+                   .HasColumnType("BIGINT")
+                   .ValueGeneratedOnAdd();
 
-                     builder.Property(e => e.Nome)
-                            .IsRequired()
-                            .HasColumnName("nome")
-                            .HasColumnType("VARCHAR")
-                            .HasMaxLength(40);
+            builder.Property(e => e.Nome)
+                   .IsRequired()
+                   .HasColumnName("nome")
+                   .HasColumnType("VARCHAR")
+                   .HasMaxLength(40);
 
-                     builder.Property(e => e.Sigla)
-                            .IsRequired()
-                            .HasColumnName("sigla")
-                            .HasColumnType("VARCHAR")
-                            .HasMaxLength(2);
+            builder.Property(e => e.Sigla)
+                   .IsRequired()
+                   .HasColumnName("sigla")
+                   .HasColumnType("VARCHAR")
+                   .HasMaxLength(2);
 
-                     builder.Property(e => e.CodigoIbge)
-                            .IsRequired(false)
-                            .HasColumnName("codigo_ibge")
-                            .HasColumnType("INTEGER");
+            builder.Property(e => e.CodigoIbge)
+                   .IsRequired(false)
+                   .HasColumnName("codigo_ibge")
+                   .HasColumnType("INTEGER");
 
-                     builder.Property(e => e.DataCadastro)
-                            .IsRequired()
-                            .HasColumnName("data_cadastro")
-                            .HasColumnType("DATE")
-                            .HasDefaultValueSql("now()");
+            builder.Property(e => e.DataCadastro)
+                   .IsRequired()
+                   .HasColumnName("data_cadastro")
+                   .HasColumnType("DATE")
+                   .HasDefaultValueSql("now()");
 
-                     builder.Property(e => e.HoraCadastro)
-                            .IsRequired()
-                            .HasColumnName("hora_cadastro")
-                            .HasColumnType("TIME WITHOUT TIME ZONE")
-                            .HasDefaultValueSql("now()");
+            builder.Property(e => e.HoraCadastro)
+                   .IsRequired()
+                   .HasColumnName("hora_cadastro")
+                   .HasColumnType("TIME WITHOUT TIME ZONE")
+                   .HasDefaultValueSql("now()");
 
-                     builder.Property(e => e.PaisId)
-                            .IsRequired()
-                            .HasColumnName("pais_id")
-                            .HasColumnType("BIGINT");
+            builder.Property(e => e.PaisId)
+                   .IsRequired()
+                   .HasColumnName("pais_id")
+                   .HasColumnType("BIGINT");
 
-                     // Relacionamento com a tabela País
-                     builder.HasOne(e => e.Pais)
-                            .WithMany()
-                            .HasForeignKey(e => e.PaisId)
-                            .OnDelete(DeleteBehavior.NoAction);
-              }
-       }
+            // Relacionamento com a tabela País
+            builder.HasOne(e => e.Pais)
+                   .WithMany()
+                   .HasForeignKey(e => e.PaisId)
+                   .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
 }
